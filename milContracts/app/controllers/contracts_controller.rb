33f3f64e006102d4791b51b37363453fbf8247c2
Bryce Lively <bryce.lively@gmail.com>
2016-01-12 18:03:33 -0500

    BigDecimals not rendering properly (0.0)

--- milContracts/app/controllers/contracts_controller.rb
+++ milContracts/app/controllers/contracts_controller.rb
@@ -1,9 +1,18 @@
 class ContractsController < ApplicationController
 	  def index
 		  @contracts = Contract.all
+		  def get_num
+			  tot = 0
+		  	@contracts.each do |a|
+			  tot += a.dollar_amt.to_f
+		  end
+			  return tot
+		  end
+		  @totalNum = get_num
 	  end
       def show
 		  @contract = Contract.find(params[:id])
+		  @money = @contract.dollar_amt.to_f
       end
       def update
       end
