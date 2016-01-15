753fd7c4845d83eb3f0e5f20182024516cf40d7a
Bryce Lively <bryce.lively@gmail.com>
2016-01-15 11:32:54 -0500

    reverted to semi-working controller-based solution

--- milC_AngularFE/js/contracts/index.controller.js
+++ milC_AngularFE/js/contracts/index.controller.js
@@ -70,6 +70,16 @@
                     pointHitDetectionRadius: 2,
                     scaleShowVerticalLines: false
                 });
+                document.getElementById('myChart').onclick = function(evt) {
+                    var activePoints = contractsOverTime.getPointsAtEvent(evt);
+                    console.log(activePoints);
+                    self.selectedElements.splice(0,self.selectedElements.length);
+                    activePoints.forEach(function(contract) {
+                        self.selectedElements.push(contract)
+                    });
+                    console.log("selectedElements: " + self.selectedElements);
+                    // => activePoints is an array of points on the canvas that are at the same position as the click event.
+                };
             })
         }, 1000);
     }
